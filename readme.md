d7f1ddac8f414a207d5375c650c6d4d7501091a3
FranciscoBraga <franciscobraga.fob@gmail>
2024-07-31 00:18:06 -0400

    inicio do projeto

---
+++ readme.md
@@ -0,0 +1,9 @@
+### objetivo
+
+Criar um carrinho de compra baseado no carrinho de compra na shoppe. aonde o carrinho armazene itens  e faça  o cálculo de sub-itens automaticamente.
+
+//dominio da aplicação : carrinho  de compras
+
+//as entidades  representadas 
+//carrinho 
+//itens
---
+++ src/services/cart.js
@@ -0,0 +1,11 @@
+// quais açoes o carrinho pode fazer
+
+//adicionar item no carrinho
+async function addItem(userCart){}
+
+//deletar um item 
+async function deleteItem(userCart, name){}
+//remover um item
+async function removeItem(userCart,index){}
+//calcula o total
+async function calculateTotal(userCart){}
